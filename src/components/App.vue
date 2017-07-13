025797f1a34c67a933f352b6af25ac6229674e5a
Narut Poovorakit <net.narut.p@gmail.com>
2017-07-13 14:43:43 +0700

    Initialize

---
+++ src/components/App.vue
@@ -0,0 +1,11 @@
+<script>
+import store from '../vuex/store'
+
+export default {
+  // injecting Vuex store into the root Vue instance
+  store,
+  ready: function () {
+    this.$vueLocalizeInit(this.$route)
+  }
+}
+</script>
